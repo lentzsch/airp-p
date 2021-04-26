31a7d3f46504bf8134855b4afdbe8efc3ba59df0
James Lentzsch <jrlentzsch@gmail.com>
2021-04-26 15:19:22 -0700

    auth me skeleton imported

---
+++ backend/db/seeders/20210423204408-demo-user.js
@@ -0,0 +1,33 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+// const faker = require('faker');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    
+      return queryInterface.bulkInsert('Users', [
+        {
+          username: 'Demo-lition',
+          email: 'demo@user.io',
+          hashedPassword: bcrypt.hashSync('password'),
+        },
+        // {
+        //   username: 'Cpt-Murphy',
+        //   email: faker.internet.email(),
+        //   hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        // },
+        // {
+        //   username: 'Lil-Marco',
+        //   email: 'demo@user.io',
+        //   hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        // },
+      ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+      const Op = Sequelize.Op;
+      return queryInterface.bulkDelete('Users', {
+        username: { [Op.in]: ['Demo-lition', 'Cpt-Murphy', 'Lil-Marco'] }
+      }, {});
+  }
+};
